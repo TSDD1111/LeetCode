1d85eb4a0b3c857872ea0952fb6beba4cfe275d9
TSDD1111 <61583771+TSDD1111@users.noreply.github.com>
2021-04-19 14:24:49 +0800

    Create containsNearbyAlmostDuplicate.java

---
+++ 桶排序/220.存在重复元素III/containsNearbyAlmostDuplicate.java
@@ -0,0 +1,37 @@
+//根据桶排序进行判断，两个数之间的大小不能超过t，所以让桶的大小为t+1，
+//如果在同一个桶里有两个元素则符合要求，
+//如果在相邻的桶里需要判断大小是否超过t
+//如果在不相邻的桶里，则一定不符合要求
+//根据上诉要求建立桶即可
+
+class Solution {
+    public boolean containsNearbyAlmostDuplicate(int[] nums, int k, int t) {
+        int n = nums.length;
+        Map<Long, Long> map = new HashMap<Long, Long>();
+        long w = (long) t + 1;
+        for (int i = 0; i < n; i++) {
+            long id = getID(nums[i], w);
+            if (map.containsKey(id)) {
+                return true;
+            }
+            if (map.containsKey(id - 1) && Math.abs(nums[i] - map.get(id - 1)) < w) {
+                return true;
+            }
+            if (map.containsKey(id + 1) && Math.abs(nums[i] - map.get(id + 1)) < w) {
+                return true;
+            }
+            map.put(id, (long) nums[i]);
+            if (i >= k) {
+                map.remove(getID(nums[i - k], w));
+            }
+        }
+        return false;
+    }
+
+    public long getID(long x, long w) {
+        if (x >= 0) {
+            return x / w;
+        }
+        return (x + 1) / w - 1;
+    }
+}
