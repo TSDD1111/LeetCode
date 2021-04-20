273bc63ca8d99261fb1f9749a943071828ac3e7d
TSDD1111 <61583771+TSDD1111@users.noreply.github.com>
2021-04-20 22:55:27 +0800

    Create Solution.java

---
+++ 每日一题/8-28.实现strStr()/Solution.java
@@ -0,0 +1,33 @@
+//通过KMP算法即可实现字符串匹配
+//或者通过java的api——indexOf即可实现
+
+class Solution {
+    public int strStr(String haystack, String needle) {
+        int n = haystack.length(), m = needle.length();
+        if (m == 0) {
+            return 0;
+        }
+        int[] pre = new int[m];
+        for (int i = 1, j = 0; i < m; i++) {
+            while (j > 0 && needle.charAt(i) != needle.charAt(j)) {
+                j = pre[j - 1];
+            }
+            if (needle.charAt(i) == needle.charAt(j)) {
+                j++;
+            }
+            pre[i] = j;
+        }
+        for (int i = 0, j = 0; i < n; i++) {
+            while (j > 0 && haystack.charAt(i) != needle.charAt(j)) {
+                j = pre[j - 1];
+            }
+            if (haystack.charAt(i) == needle.charAt(j)) {
+                j++;
+            }
+            if (j == m) {
+                return i - m + 1;
+            }
+        }
+        return -1;
+    }
+}
